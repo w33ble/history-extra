2c26fbad5f0ad67862035ed04f78f0482e3d7dc1
joe fleming <joe.fleming@gmail.com>
2018-11-14 20:36:22 -0700

    test: add tests for push/replace on same path

--- test/lib/utils.js
+++ test/lib/utils.js
@@ -1,42 +1,11 @@
 /* eslint no-param-reassign: 0 */
 import getHistory from './getHistory';
 
-const waitTime = 1000;
-
 export function listenOnce(cb) {
   const unlisten = getHistory().listen((location, action) => {
     cb(location, action);
     unlisten();
   });
-}
-
-const nav = method => (path, state, cb) => {
-  if (typeof state === 'function') {
-    cb = state;
-    state = null;
-  }
-
-  // listen for change once, call cb when done, passes (location, action)
-  listenOnce(cb);
-
-  setTimeout(() => {
-    getHistory()[method](path, state);
-  }, waitTime);
-};
-
-export function slowPush(path, state, cb) {
-  return nav('push')(path, state, cb);
-}
-
-export function slowReplace(path, state, cb) {
-  return nav('replace')(path, state, cb);
-}
-
-export function slowGo(dir, cb) {
-  window.history.go(dir);
-  setTimeout(() => {
-    cb();
-  }, waitTime);
 }
 
 export function guid() {
